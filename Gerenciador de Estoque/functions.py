69676f54a6c9881d33e4227e2cafc4e7cecfd12b
WeslleySantiagoo <weslleywgosgabriel@gmail.com>
2025-02-05 23:20:00 -0300

    Autenticação com Database

--- Gerenciador de Estoque/functions.py
+++ Gerenciador de Estoque/functions.py
@@ -1,1 +1,88 @@
 #pendente
+import pyrebase
+from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+
+config = {
+    'apiKey': "AIzaSyAJlxRR9YogNG0Hnztn618x8I7pAQVoCZc",
+'authDomain': "gerenciador-de-estoque-pisi-i.firebaseapp.com",
+'databaseURL': "https://gerenciador-de-estoque-pisi-i-default-rtdb.firebaseio.com",
+'projectId': "gerenciador-de-estoque-pisi-i",
+'storageBucket': "gerenciador-de-estoque-pisi-i.firebasestorage.app",
+'messagingSenderId': "148937568721",
+'appId': "1:148937568721:web:95b0e583f8ee82538230f0",
+'easurementId': "G-2001V6GRXW"
+}
+fb = pyrebase.initialize_app(config)
+
+
+def sign_in_db(email, password):
+    if len(email) > 9:
+        if email[-9:] == '@ufrpe.br':
+            if len(password) >= 6:
+                auth = fb.auth()
+                try:
+                    user = auth.sign_in_with_email_and_password(email, password)
+                    return "Logado com sucesso"
+                except:
+                    print(Exception)
+                    return 'Email ou senha incorreto.'
+            else:
+                return 'Senha deve conter mais de 6 dígitos'
+        else:
+            return 'Dominio incorreto'
+    else:
+        return 'Email pequeno demais'
+    
+
+def sign_up_db(email, password):
+    if len(email) > 9:
+        if email[-9:] == '@ufrpe.br':
+            if len(password) >= 6:
+                auth = fb.auth()
+                try:
+                    user = auth.create_user_with_email_and_password(email, password)
+                    return "Conta criada com sucesso"
+                except:
+                    return "O email já está cadastrado"
+            return 'Senha deve conter mais de 6 dígitos'
+        else:
+            return 'Dominio incorreto'
+    else:
+        return 'Email pequeno demais'
+        
+
+def open_popup_sign_in(msg):
+        popup = Popup(
+            title="Sign In",
+            size_hint=(None, None),
+            size=(300, 150),
+            background_color=(1,1,1,1)
+        )
+        fechar_btn = Button(text="Fechar")
+        fechar_btn.bind(on_press=popup.dismiss)
+        layout = BoxLayout(orientation='vertical')
+        layout.add_widget(Label(text=msg))
+        layout.add_widget(fechar_btn)
+        popup.add_widget(layout)
+        popup.open()
+
+
+def open_popup_sign_up(msg):
+        popup = Popup(
+            title="Sign Up",
+            size_hint=(None, None),
+            size=(300, 150),
+            background_color=(1,1,1,1)
+        )
+
+        fechar_btn = Button(text="Fechar")
+        fechar_btn.bind(on_press=popup.dismiss)
+
+        layout = BoxLayout(orientation='vertical')
+        layout.add_widget(Label(text=msg))
+        layout.add_widget(fechar_btn)
+        popup.add_widget(layout)
+        popup.open()
--- Gerenciador de Estoque/main.py
+++ Gerenciador de Estoque/main.py
@@ -3,25 +3,32 @@ from kivy.lang import Builder
 from kivy.core.window import Window
 from kivymd.uix.screenmanager import ScreenManager
 from kivy.uix.filechooser import Screen
-from functions import *
+from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.core.text import LabelBase
+import functions
+import pyrebase
 
-class JanelaGerenciadora(ScreenManager):
-    pass
+global db
+
+config = {
+    'apiKey': "AIzaSyAJlxRR9YogNG0Hnztn618x8I7pAQVoCZc",
+'authDomain': "gerenciador-de-estoque-pisi-i.firebaseapp.com",
+'databaseURL': "https://gerenciador-de-estoque-pisi-i-default-rtdb.firebaseio.com",
+'projectId': "gerenciador-de-estoque-pisi-i",
+'storageBucket': "gerenciador-de-estoque-pisi-i.firebasestorage.app",
+'messagingSenderId': "148937568721",
+'appId': "1:148937568721:web:95b0e583f8ee82538230f0",
+'easurementId': "G-2001V6GRXW"
+}
+LabelBase.register(name='Poppins', fn_regular='Fonts/Poppins-Bold.ttf')
+fb = pyrebase.initialize_app(config)
+db = fb.database()
+
 
 class LoginScreen(Screen):
-    pass
-
-class EstoqueScreen(Screen):
-    pass
-
-class MainApp(MDApp):
-    def build(self):
-        Window.size = (360, 640)
-        self.title = "Genciador de Estoque"
-        self.theme_cls.primary_palette = "Green"
-        self.theme_cls.theme_style = "Light"
-        return Builder.load_file('telas.kv')
-            
     def verify_dominion_email(self, email_field):
             if email_field.text != "":
                 email_field.error = not email_field.text.endswith('@ufrpe.br')
@@ -33,6 +40,39 @@ class MainApp(MDApp):
             
     def verify_password(self, password_field):
         password_field.error = self.invalid_password(password_field.text) != False
-        
 
+
+    def sign_in_button(self, email, password):
+        sign_in = functions.sign_in_db(email, password)
+        # if sign_in == "Logado com sucesso":
+        #     self.manager.current='Tela de Estoque'
+        self.manager.current='Tela de Estoque'
+        return self.open_popup_sign_in(sign_in)
+    
+
+    def sign_up_button(self, email, password):
+        sign_up = functions.sign_up_db(email, password)
+        return self.open_popup_sing_up(sign_up)
+    
+    
+    def open_popup_sign_in(self, msg):
+        return functions.open_popup_sign_in(msg)
+    
+
+    def open_popup_sing_up(self, msg):
+        return functions.open_popup_sign_up(msg) 
+    pass
+
+class EstoqueScreen(Screen):
+
+    pass
+
+class MainApp(MDApp):
+    dialog = None
+    def build(self):
+        Window.size = (360, 640)
+        self.title = "Genciador de Estoque"
+        self.theme_cls.primary_palette = 'Green'
+        self.theme_cls.theme_style = "Light"
+        return Builder.load_file('telas.kv')
 MainApp().run()
